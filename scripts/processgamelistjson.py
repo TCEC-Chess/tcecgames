d110459e66a5a68e51762298071c3649294a9c19
Sami Kiminki <skiminki@users.noreply.github.com>
2022-10-29 13:21:09 +0300

    Add S23 Infrafinal

--- scripts/processgamelistjson.py
+++ scripts/processgamelistjson.py
@@ -102,7 +102,7 @@ def classify_event(season, event_id, event_name):
         if int(season) <= 19:
             return "BONUS"
 
-        if event_id in ["s21divisionif", "s22divisionif"]:
+        if event_id in ["s21divisionif", "s22divisionif", "s23divisionif"]:
             return "MAIN"
 
         if event_id in ["s21divisionseev", "s21divisioneval", "s21divisionlc0cp", "s22divisionse2"]:
