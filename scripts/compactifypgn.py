0113aced431512f2187a686d74d26c6e1ac42f86
Sami Kiminki <46296557+skiminki@users.noreply.github.com>
2024-03-17 21:40:53 +0200

    Add Thoresen tournament and match support in compactifypgn
    Fixes #41

--- scripts/compactifypgn.py
+++ scripts/compactifypgn.py
@@ -9,20 +9,28 @@ def compactify_game(game, num):
     game.comment = None
     last_book_node = game
     non_book_node_found = False
+    thoresen2 = False
 
     # check that we have the "Site" header
     if not "Site" in game.headers:
         print(f"Error in game {num}: Missing tag 'Site'", file=sys.stderr)
         sys.exit(2)
 
+    if game.headers["Site"].startswith('https://tcec-chess.com/#season=0&div=thort2&'):
+        thoresen2 = True
+    if game.headers["Site"].startswith('https://tcec-chess.com/#season=0&div=thorm1&'):
+        thoresen2 = True
+    if game.headers["Site"].startswith('https://tcec-chess.com/#season=0&div=thorm2&'):
+        thoresen2 = True
+    if game.headers["Site"].startswith('https://tcec-chess.com/#season=0&div=thorm3&'):
+        thoresen2 = True
+
     # note: we start from the root node which is one before the first move
     node = game
     while True:
-
         # next mainline move
         if (len(node.variations) > 0):
             # strip every other variation except the mainline
-            #node.variations = node.variations[0:1]
             node = node.variations[0]
         else:
             node = None
@@ -30,18 +38,34 @@ def compactify_game(game, num):
 
         # check if this is a book node
         if not non_book_node_found:
-
             comment = node.comment
             if comment is None:
                 comment = ""
 
-            # the very old style (seasons 1..5)
-            if comment.startswith("ev=0.00, d=1, mt=00:00:00"):
-                last_book_node = node
-            elif comment == "book" or comment.startswith("book, "): # the modern
-                last_book_node = node
+            if not thoresen2:
+                # the ancient style (Thoresen tournament 1)
+                if comment == "B 0":
+                    last_book_node = node
+                # thoresen specials
+                elif comment == "ev=0,d=1,tl=02:00:00" or comment == "Eval = 0, Depth = 1, TimeLeft = 02:00:00" or \
+                     comment == "Eval = 0.00, Depth = 1, TimeLeft = 02:00:00" or comment == "ev=0.00,d=1,tl=02:00:00":
+                    last_book_node = node
+                elif comment.startswith("Eval = 0.00, Depth = 1, MoveTime = 00:00:00"):
+                    print(f"Thoresen special", file=sys.stderr)
+                    last_book_node = node
+                    # the very old style (seasons 1..5)
+                elif comment.startswith("ev=0.00, d=1, mt=00:00:00"):
+                    last_book_node = node
+                elif comment == "book" or comment.startswith("book, "): # the modern
+                    last_book_node = node
+                else:
+                    non_book_node_found = True
             else:
-                non_book_node_found = True
+                # thoresen tournament 2 style, also used in match 1-3
+                if comment == "":
+                    last_book_node = node
+                else:
+                    non_book_node_found = True
 
         node.comment = None
 
