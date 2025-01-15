1e8133532bfa42eff16e27d44291b0bc89c9ba0b
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2025-01-15 15:34:55 +0100

    deal with more corner cases

--- scripts/fix-engine-names.py
+++ scripts/fix-engine-names.py
@@ -10,8 +10,7 @@ import sys
 exceptionalNames = {
     # format: <player tag> : (<engine name>, <engine version>, <optional special tag>)
     "Koivisto2 4.41": ("Koivisto", "4.41", "2"),
-    "MrBob 1.0.0_dev": ("Mr_Bob", "1.0.0_dev"),
-    "Stockfish0_1pct 20191225": ("Stockfish0.1pct", "20191225"),
+    "MrBob 1.0.0_dev": ("Mr_Bob", "1.0.0_dev", ""),
     "Weiss 0.10-dev-20200525 1": ("Weiss", "0.10-dev-20200525", "1"),
     "Weiss 0.10-dev-20200525 2": ("Weiss", "0.10-dev-20200525", "2"),
     "Crafty_25.2_CCRL 64-bit 4CPU": ("Crafty", "25.2", "CCRL 64-bit 4CPU"),
@@ -22,6 +21,7 @@ exceptionalNames = {
     "Stockfish_11_CCRL 64-bit_4CPU": ("Stockfish", "11", "CCRL 64-bit 4CPU"),
     "Stockfish_13_CCRL 64-bit_4CPU": ("Stockfish", "13", "CCRL 64-bit 4CPU"),
     "Stockfish_15_CCRL 64-bit_4CPU": ("Stockfish", "15", "CCRL 64-bit 4CPU"),
+    "Chat": ("TCEC Chat", "", ""),
 }
 
 
@@ -33,8 +33,8 @@ def addSwissTestStockfishVersions():
             f"{i:02}",
         )
         exceptionalNames[f"{i:02}StockfishClassical 202007311012"] = (
-            "StockfishClassical",
-            "202007311012",
+            "Stockfish",
+            "202007311012_Classical",
             f"{i:02}",
         )
 
@@ -64,17 +64,84 @@ def addSufiTaggedEngines():
         exceptionalNames[name2] = i
 
 
+def addFixedStockfish15():
+    # change "Stockfish_15_100M" to "Stockfish_100M (15)" etc.
+    for i in [1, 3, 10, 30, 100, 300]:
+        for s in ["k", "M", "G"]:
+            suffix = f"{i}{s}"
+            name = "Stockfish_15_" + suffix
+            newname = "Stockfish_" + suffix
+            exceptionalNames[name] = (newname, "15", "")
+    exceptionalNames["Stockfish_15_300M scaled_to_450Mnodes"] = (
+        "Stockfish_300M",
+        "15",
+        "scaled to 450Mnodes",
+    )
+    exceptionalNames["Renamedfish_15_3M"] = (
+        "Stockfish_3M",
+        "15",
+        "renamed",  # or "copy"
+    )
+
+
+def amendEngineNames(player):
+    name, space, rest = player.partition(" ")
+    name = name.replace("Chess22k", "chess22k")
+    name = re.sub(r"[lL]c0", "LCZero", name)
+    # replace underscore with digital point
+    name = name.replace("0_1pct", "0.1pct")
+    # include an underscore before 0.1pct, 10pct, 30pct etc
+    name = re.sub(r"([a-zA-Z])(\d+(\.\d+)?pct)", r"\1_\2", name)
+    # rename kn to k
+    name = re.sub(r"([0-9])(kn)", r"\1k", name)
+    # rename Mn to M
+    name = re.sub(r"([0-9])(Mn)", r"\1M", name)
+    # include an underscore before 1n, 100k, 10M, 1G etc
+    if name not in ["chess22k", "we4k"]:
+        name = re.sub(r"([a-zA-Z])(\d+(\d+)?[nkMG])", r"\1_\2", name)
+    name = name.replace("Stockfishd1", "Stockfish_d1")
+    name = name.replace("StockfishDepth", "Stockfish_d")
+    name = name.replace("Stockfish1thread", "Stockfish_1thread")
+    return name + space + rest
+
+
+def fixCopyTags(t):
+    name, version, tag = t
+    if name.endswith(" copy"):
+        name, c, _ = name.rpartition(" copy")
+        tag = "copy" if tag == "" else tag + "_copy"
+    if version.endswith("_copy"):
+        version, c, _ = version.rpartition("_copy")
+        tag = "copy" if tag == "" else tag + "_copy"
+    if version == "copy":
+        version = ""
+        tag = "copy" if tag == "" else tag + "_copy"
+    return name, version, tag
+
+
+def fixMemoryAndThreadTags(t):
+    name, version, tag = t
+    if version in ["64GiB", "128GiB", "256GiB", "256th"]:
+        tag = version
+        name, _, version = name.rpartition(" ")
+    return name, version, tag
+
+
+def fixVariousTags(t):
+    name, version, tag = t
+    if version in ["interleave", "localalloc", "none"]:
+        tag = version
+        name, _, version = name.rpartition(" ")
+    return name, version, tag
+
+
 def serializeNameVersionTriplet(t):
-    if len(t) == 1:
-        return t[0]
-    elif len(t) == 2:
-        return f"{t[0]} ({t[1]})"
-    else:
-        return f"{t[0]} ({t[1]}) [{t[2]}]"
+    s1 = "" if t[1] == "" else f" ({t[1]})"
+    s2 = "" if t[2] == "" else f" [{t[2]}]"
+    return f"{t[0]}{s1}{s2}"
 
 
 def fixEngineNames():
-
     playerTagMatcher = re.compile(r'\[(White|Black) "([^"]*)"\]', re.ASCII)
 
     regularNameVersionMatcher = re.compile(r"(.*) ([^ ]+)", re.ASCII)
@@ -86,15 +153,21 @@ def fixEngineNames():
         if playerTagMatch:
             player = playerTagMatch.group(2)
 
+            player = amendEngineNames(player)
+
             if player in exceptionalNames:
                 nameVersionTriplet = exceptionalNames[player]
 
             elif regularNameVersionMatcher.fullmatch(player):
                 m = regularNameVersionMatcher.fullmatch(player)
-                nameVersionTriplet = (m.group(1), m.group(2))
+                nameVersionTriplet = (m.group(1), m.group(2), "")
 
             else:
-                nameVersionTriplet = (player,)
+                nameVersionTriplet = (player, "", "")
+
+            nameVersionTriplet = fixCopyTags(nameVersionTriplet)
+            nameVersionTriplet = fixMemoryAndThreadTags(nameVersionTriplet)
+            nameVersionTriplet = fixVariousTags(nameVersionTriplet)
 
             print(
                 f'[{playerTagMatch.group(1)} "{serializeNameVersionTriplet(nameVersionTriplet)}"]'
@@ -113,6 +186,7 @@ def main(argv):
 
     addSwissTestStockfishVersions()
     addSufiTaggedEngines()
+    addFixedStockfish15()
 
     fixEngineNames()
 
