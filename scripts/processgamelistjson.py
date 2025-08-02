e9a43d1d646af5a0c1218a06a01437b361ac64a2
Sami Kiminki <skiminki@users.noreply.github.com>
2025-08-02 13:29:05 +0300

    Add S28 L1 + L2 and testing events

--- scripts/processgamelistjson.py
+++ scripts/processgamelistjson.py
@@ -153,6 +153,9 @@ def classify_event(season, event_id, event_name):
         if event_id in ["s28division4kf"]:
             return "MAIN"
 
+        if event_id in ["s28divisionl1t1"]:
+            return "MAIN"
+
     fatal(f"Don't know how to classify Season '{season}', event id '{event_id}', event name '{event_name}'", 5)
 
 def sync_pgn(pgnfile):
